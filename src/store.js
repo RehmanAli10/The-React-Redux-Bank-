febd7b3712112b33752d67e17a1852df286ec982
Rehman Ali <vrehmanali@gmail.com>
2024-02-14 12:12:05 +0500

    created store using configureStore

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer);
 
 export default store;
